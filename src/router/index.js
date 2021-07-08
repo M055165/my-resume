5de70e5b663f0e40650a3df071730f6bdf077054
m055165 <michael50601@gmail.com>
2021-07-08 21:23:17 +0800

    origin

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import {  createRouter, createWebHashHistory } from "vue-router";
 import Home from '../views/Home.vue'
 
 
@@ -12,7 +12,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHashHistory,
+    history: createWebHashHistory(),
     routes,
   });
   

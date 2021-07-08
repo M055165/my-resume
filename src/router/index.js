70e734f3995990a2ef129a52423cc57a157e7a2f
m055165 <michael50601@gmail.com>
2021-07-08 21:19:18 +0800

    deploy

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createWebHistory, createRouter } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from '../views/Home.vue'
 
 
@@ -12,7 +12,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory,
     routes,
   });
   

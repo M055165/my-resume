e3ad972056198b7666c1dc72cd915479fb1d7048
蔡竣智 <tc055165@llt.com.tw>
2024-04-23 16:37:38 +0800

    add footer com

---
+++ src/components/footerCom.vue
@@ -0,0 +1,36 @@
+<template>
+    <footer>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius dolorem minima quis sit unde magni deleniti odio,
+        fugit perspiciatis quia excepturi? Non perspiciatis distinctio rerum nihil ducimus culpa a illum.
+        <v-icon class="mr-8 mt-1 icon" @click="scrollToTop">mdi-arrow-up-bold-circle-outline</v-icon>
+    </footer>
+</template>
+
+
+<script setup>
+
+const scrollToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" // 滑動行為，平滑滾動
+    });
+}
+
+
+</script>
+
+<style scoped>
+footer {
+    min-height: 300px;
+    background-color: rgba(0, 0, 0, 0.7);
+    position: relative;
+}
+
+.icon {
+    color: white;
+    font-size: 50px;
+    position: absolute;
+    top: -25px;
+    left: calc(50% - 25px);
+}
+</style>
--- src/views/homeView.vue
+++ src/views/homeView.vue
@@ -2,12 +2,14 @@
   <NavbarCom />
   <InfoCom />
   <TimelineCom />
+  <FooterCom />
 </template>
 
 <script setup>
 import NavbarCom from '@/components/navbarCom.vue'
 import InfoCom from '@/components/infoCom.vue';
 import TimelineCom from '@/components/timelineCom.vue'
+import FooterCom from '@/components/footerCom.vue'
 </script>
 
 

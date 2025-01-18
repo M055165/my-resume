17d1834142d7b7c008db776afbf813d70eeea0a9
eddie055165 <eddie055165@gmail.com>
2025-01-19 03:09:37 +0800

    update base;

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    
     vue({ 
       template: { transformAssetUrls }
     }),
@@ -35,4 +36,5 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  base: './',
 })
